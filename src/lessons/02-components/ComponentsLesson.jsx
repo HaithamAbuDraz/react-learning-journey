d41a26e1ee6048a60366db6c0ed47ad4d27a52e3
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-14 11:47:43 +0300

    Add UserStats component into ComponentsLesson

--- src/lessons/02-components/ComponentsLesson.jsx
+++ src/lessons/02-components/ComponentsLesson.jsx
@@ -1,12 +1,14 @@
-import Footer from './components/Footer';
 import Header from './components/Header';
 import ProfileCard from './components/ProfileCard';
+import UserStats from '../../challenges/02-components/UserStats';
+import Footer from './components/Footer';
 
 function ComponentsLesson() {
   return (
     <div>
       <Header />
       <ProfileCard />
+      <UserStats />
       <Footer />
     </div>
   );
