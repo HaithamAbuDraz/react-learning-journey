e062ea59f84674337b56629227df4a53ace1a55f
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-14 14:31:15 +0300

    Add children prop examples to PropsLesson with Card component

--- src/lessons/03-props/PropsLesson.jsx
+++ src/lessons/03-props/PropsLesson.jsx
@@ -1,10 +1,13 @@
 import ProfileCard from '../../challenges/03-props/ProfileCard';
 import SkillsCard from '../../challenges/03-props/SkillsCard';
+import Card from '../../challenges/03-props/Card';
 
 function PropsLesson() {
   return (
     <div>
       <h1>Props Lesson</h1>
+
+      <h2>Profile Cards</h2>
       <ProfileCard
         name='Haitham Abu Draz'
         role='Software Engineer'
@@ -17,6 +20,19 @@ function PropsLesson() {
       <SkillsCard skill='CSS' level='Advanced' />
       <SkillsCard skill='JavaScript' level='Intermediate' />
       <SkillsCard skill='React' level='Beginner' />
+
+      <h2>Card with Children Prop</h2>
+      <Card>
+        <h2>Hello React</h2>
+        <p>This content comes from children.</p>
+        <p>The Card component wraps everything inside it!</p>
+      </Card>
+
+      <Card>
+        <h3>Another Card Example</h3>
+        <p>You can put ANY content inside a Card component</p>
+        <button>Click Me</button>
+      </Card>
     </div>
   );
 }
