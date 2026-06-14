f55f9366f3cebcc72219bb5b8171033f433b5232
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-14 11:10:31 +0300

    Add ProfileCard component for user information display

---
+++ src/lessons/02-components/components/ProfileCard.jsx
@@ -0,0 +1,11 @@
+function ProfileCard() {
+  return (
+    <div>
+      <h2>Haitham Abu Draz</h2>
+      <p>Software Engineer</p>
+      <p>Frontend Developer</p>
+    </div>
+  );
+}
+
+export default ProfileCard;
