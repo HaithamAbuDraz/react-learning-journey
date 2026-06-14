e7a0141e4fc44032e5c6cad00c61fa1e14ac55a3
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-14 11:11:52 +0300

    Add ComponentsLesson component composing Header, ProfileCard, and Footer

---
+++ src/lessons/02-components/ComponentsLesson.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import ProfileCard from './components/ProfileCard';
+
+function ComponentsLesson() {
+  return (
+    <div>
+      <Header />
+      <ProfileCard />
+      <Footer />
+    </div>
+  );
+}
+
+export default ComponentsLesson;
