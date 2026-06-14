644495e8987a017fcb41067ae0e972823303d9ce
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-14 13:36:25 +0300

    Add experience props to ProfileCard instances

--- src/lessons/03-props/PropsLesson.jsx
+++ src/lessons/03-props/PropsLesson.jsx
@@ -1,4 +1,4 @@
-import ProfileCard from './components/ProfileCard';
+import ProfileCard from '../../challenges/03-props/ProfileCard';
 
 function PropsLesson() {
   return (
@@ -8,16 +8,19 @@ function PropsLesson() {
         name='Haitham Abu Draz'
         role='Software Engineer'
         location='Palestine'
+        experience={2}
       />
       <ProfileCard
         name='Ahmed Khaled'
         role='Frontend Developer'
         location='Jordan'
+        experience={2}
       />
       <ProfileCard
         name='Anas Batra'
         role='Backend Developer'
         location='Egypt'
+        experience={1}
       />
     </div>
   );
