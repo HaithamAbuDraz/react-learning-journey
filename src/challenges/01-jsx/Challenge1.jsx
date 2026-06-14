ec06463a7107cba06227e053295d57e8c54ddd54
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-14 10:29:01 +0300

    Add Challenge1 - JSX expressions with variables

---
+++ src/challenges/01-jsx/Challenge1.jsx
@@ -0,0 +1,22 @@
+function Challenge1() {
+  const firstName = 'Haitham';
+  const lasstName = 'Abu Draz';
+  const university = 'University of Palestine';
+  const major = 'Software Engineering';
+
+  return (
+    <div>
+      <h1>Challenge 1</h1>
+
+      <p>
+        Hello, {firstName} {lasstName}!
+      </p>
+
+      <p>University: {university}</p>
+
+      <p>Major: {major}</p>
+    </div>
+  );
+}
+
+export default Challenge1;
