37d2d2073d2fcbb35ddc8d7273ca931d7afea242
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-14 11:47:20 +0300

    Add UserStats component for developer metrics display

---
+++ src/challenges/02-components/UserStats.jsx
@@ -0,0 +1,11 @@
+function UserStats() {
+  return (
+    <div>
+      <p>Projects: 10</p>
+      <p>Experience: 2 Years</p>
+      <p>Location: Palestine</p>
+    </div>
+  );
+}
+
+export default UserStats;
