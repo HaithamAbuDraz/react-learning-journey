70ae95e93a81e8460968ce65ec3f0c598e405069
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-15 15:42:22 +0300

    Add EventsLesson component to demonstrate React event handling

---
+++ src/lessons/05-events/EventsLesson.jsx
@@ -0,0 +1,23 @@
+import ClickCounter from './components/ClickCounter';
+import InputTracker from './components/InputTracker';
+import KeyTracker from './components/KeyTracker';
+
+function EventsLesson() {
+  return (
+    <div>
+      <h1>Events Lesson</h1>
+
+      <ClickCounter />
+
+      <hr />
+
+      <InputTracker />
+
+      <hr />
+
+      <KeyTracker />
+    </div>
+  );
+}
+
+export default EventsLesson;
