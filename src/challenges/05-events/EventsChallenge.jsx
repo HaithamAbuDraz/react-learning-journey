a8c23c858122b17c0e884630f8019976e2288c6c
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-15 17:26:32 +0300

    Add EventsChallenge component to wrap ColorSwitcher

---
+++ src/challenges/05-events/EventsChallenge.jsx
@@ -0,0 +1,13 @@
+import ColorSwitcher from './components/ColorSwitcher';
+
+function EventsChallenge() {
+  return (
+    <div>
+      <h1>State Challenge</h1>
+
+      <ColorSwitcher />
+    </div>
+  );
+}
+
+export default EventsChallenge;
