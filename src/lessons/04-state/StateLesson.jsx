2f951a49e3d55ce819e3af17deba238b26b0df91
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-15 13:39:56 +0300

    Add StateLesson component to demonstrate useState hook

---
+++ src/lessons/04-state/StateLesson.jsx
@@ -0,0 +1,13 @@
+import Counter from './components/Counter';
+
+function StateLesson() {
+  return (
+    <div>
+      <h1>State Lesson</h1>
+
+      <Counter />
+    </div>
+  );
+}
+
+export default StateLesson;
