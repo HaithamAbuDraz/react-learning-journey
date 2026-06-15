389706b45788377ac8019d61bdc82611b050e482
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-15 14:06:56 +0300

    Add StateChallenge component wrapping Counter

---
+++ src/challenges/04-state/StateChallenge.jsx
@@ -0,0 +1,13 @@
+import Counter from '../../challenges/04-state/components/Counter';
+
+function StateChallenge() {
+  return (
+    <div>
+      <h1>State Challenge</h1>
+
+      <Counter />
+    </div>
+  );
+}
+
+export default StateChallenge;
