7e68841dc5dd0cf177b7c96d06f13301c145a7ba
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-16 17:47:53 +0300

    Replace StudentCard with ConditionalList in ListsAndKeysChallenge

--- src/challenges/07-lists-and-keys/ListsAndKeysChallenge.jsx
+++ src/challenges/07-lists-and-keys/ListsAndKeysChallenge.jsx
@@ -1,41 +1,14 @@
-import StudentCard from './components/StudentCard';
+import ConditionalList from './components/ConditionalList';
 
 function ListsAndKeysChallenge() {
-  const students = [
-    {
-      id: 1,
-      name: 'Haitham',
-      grade: 97,
-    },
-    {
-      id: 2,
-      name: 'Ali',
-      grade: 88,
-    },
-    {
-      id: 3,
-      name: 'Omar',
-      grade: 76,
-    },
-    {
-      id: 4,
-      name: 'Layla',
-      grade: 92,
-    },
-  ];
-
+  
   return (
     <div>
       <h1>Lists And Keys Challenge</h1>
 
-      <h2>Students</h2>
-      {students.map((student) => (
-        <StudentCard
-          key={student.id}
-          name={student.name}
-          grade={student.grade}
-        />
-      ))}
+      <h2>Users</h2>
+      
+      <ConditionalList />
     </div>
   );
 }
