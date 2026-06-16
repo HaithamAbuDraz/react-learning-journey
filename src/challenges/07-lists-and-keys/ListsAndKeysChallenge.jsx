dedd2dfb99d84ac8b149b2d0228aa1c880b710e9
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-16 17:18:18 +0300

    Add ListsAndKeysChallenge wrapper for SkillsList

---
+++ src/challenges/07-lists-and-keys/ListsAndKeysChallenge.jsx
@@ -0,0 +1,13 @@
+import SkillsList from "./components/SkillsList";
+
+function ListsAndKeysChallenge() {
+  return(
+    <div>
+      <h1>Lists And Keys Challenge</h1>
+
+      <SkillsList />
+    </div>
+  );
+}
+
+export default ListsAndKeysChallenge;
