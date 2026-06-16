dfd82bedc5c6b8c51112b7eb985842c97eb91af7
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-16 11:37:13 +0300

    Add ConditionalRenderingChallenge wrapper for AgeChecker

---
+++ src/challenges/06-conditional-rendering/ConditionalRenderingChallenge.jsx
@@ -0,0 +1,13 @@
+import AgeChecker from './components/AgeChecker';
+
+function ConditionalRenderingChallenge() {
+  return (
+    <div>
+      <h1>Conditional Rendering Challenge</h1>
+
+      <AgeChecker />
+    </div>
+  );
+}
+
+export default ConditionalRenderingChallenge;
