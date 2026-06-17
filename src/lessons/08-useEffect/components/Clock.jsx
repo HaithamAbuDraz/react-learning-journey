98ffb46cba25eeb8ac19a9558cd42f056de8649c
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-17 16:49:52 +0300

    Add Clock component with useEffect for interval and cleanup

---
+++ src/lessons/08-useEffect/components/Clock.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>{time.toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+
+export default Clock;
