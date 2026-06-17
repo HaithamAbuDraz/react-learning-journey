45ad72034369f9b497ae06836064c93bed2d9b45
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-17 15:31:16 +0300

    Add UseEffectLesson wrapper for PageTitle component

---
+++ src/lessons/08-useEffect/UseEffectLesson.jsx
@@ -0,0 +1,13 @@
+import PageTitle from './components/PageTitle'
+
+function UseEffectLesson () {
+  return (
+    <div>
+      <h1>Use Effect Lesson</h1>
+
+      <PageTitle />
+    </div>
+  );
+}
+
+export default UseEffectLesson;
