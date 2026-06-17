d3cc81d1e37a44ee067ca30bd77aa8c7af3ecefe
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-17 11:22:02 +0300

    Replace ConditionalList with TodoList in ListsAndKeysChallenge

--- src/challenges/07-lists-and-keys/ListsAndKeysChallenge.jsx
+++ src/challenges/07-lists-and-keys/ListsAndKeysChallenge.jsx
@@ -1,14 +1,10 @@
-import ConditionalList from './components/ConditionalList';
+import TodoList from './components/TodoList';
 
 function ListsAndKeysChallenge() {
   
   return (
-    <div>
-      <h1>Lists And Keys Challenge</h1>
-
-      <h2>Users</h2>
-      
-      <ConditionalList />
+    <div>      
+      <TodoList />
     </div>
   );
 }
