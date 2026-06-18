5d715d8672c25e862cdc549f44db32b4715b0d91
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-18 12:45:13 +0300

    Add UseEffectChallenge wrapper for BackgroundChanger

---
+++ src/challenges/08-useEffect/UseEffectChallenge.jsx
@@ -0,0 +1,12 @@
+import BackgroundChanger from './components/BackgroundChanger';
+
+function UseEffectChallenge() {
+  
+  return (
+    <div>      
+      <BackgroundChanger />
+    </div>
+  );
+}
+
+export default UseEffectChallenge;
