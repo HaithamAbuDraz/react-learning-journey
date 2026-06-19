21b3c993f8295b875be22f9d16f10980fc5b9b15
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-19 15:14:41 +0300

    Add ApiCallsLesson wrapper for UserFetcher

---
+++ src/lessons/09-api-calls/ApiCallsLesson.jsx
@@ -0,0 +1,13 @@
+import UserFetcher from './components/UserFetcher';
+
+function ApiCallsLesson() {
+  return (
+    <div>
+      <h1>API Calls Lesson</h1>
+
+      <UserFetcher />
+    </div>
+  );
+}
+
+export default ApiCallsLesson;
