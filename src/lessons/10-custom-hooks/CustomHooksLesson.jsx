f908459be94a26a36f89a7d5715c5cee11853629
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-20 16:20:41 +0300

    Add CustomHooksLesson wrapper for Counter

---
+++ src/lessons/10-custom-hooks/CustomHooksLesson.jsx
@@ -0,0 +1,13 @@
+import Counter from './components/Counter';
+
+function CustomHooksLesson() {
+  return (
+    <div>
+      <h1>Custom Hooks Lesson</h1>
+
+      <Counter />
+    </div>
+  );
+}
+
+export default CustomHooksLesson;
