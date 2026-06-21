4253dc20aac6ba57cc13f8e7af8672aebca2626b
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-21 16:43:14 +0300

    Add CustomHooksChallenge wrapper for ToggleDemo

---
+++ src/challenges/10-custom-hooks/CustomHooksChallenge.jsx
@@ -0,0 +1,13 @@
+CustomHooksChallenge;
+
+import ToggleDemo from './components/ToggleDemo';
+
+function CustomHooksChallenge() {
+  return (
+    <div>
+      <ToggleDemo />
+    </div>
+  );
+}
+
+export default CustomHooksChallenge;
