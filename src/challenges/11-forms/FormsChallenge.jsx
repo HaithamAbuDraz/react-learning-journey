1fc27c4867fb8422d14f0a0b4b52bcee822d2cf5
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-22 12:14:35 +0300

    Add FormsChallenge wrapper for CharacterCounter

---
+++ src/challenges/11-forms/FormsChallenge.jsx
@@ -0,0 +1,11 @@
+import CharacterCounter from './components/CharacterCounter';
+
+function FormsChallenge() {
+  return (
+    <div>
+      <CharacterCounter />
+    </div>
+  );
+}
+
+export default FormsChallenge;
