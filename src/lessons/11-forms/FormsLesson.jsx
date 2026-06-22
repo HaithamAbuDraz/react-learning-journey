2f2f1e105625c5132e866f2ceac47feca6fb4cb0
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-22 11:03:16 +0300

    Add FormsLesson wrapper for BasicForm

---
+++ src/lessons/11-forms/FormsLesson.jsx
@@ -0,0 +1,13 @@
+import BasicForm from './components/BasicForm';
+
+function FormsLesson() {
+  return (
+    <div>
+      <h1>Forms Lesson</h1>
+
+      <BasicForm />
+    </div>
+  );
+}
+
+export default FormsLesson;
