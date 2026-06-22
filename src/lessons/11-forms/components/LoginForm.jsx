c96283cfe084223bd2f46533d1a115c1b9c19732
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-22 11:19:14 +0300

    Add LoginForm component with multiple controlled inputs

---
+++ src/lessons/11-forms/components/LoginForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login Form</h2>
+
+      <input
+        type='text'
+        placeholder='Enter your email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type='password'
+        placeholder='Enter your password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button type='submit'>Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
