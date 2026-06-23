88658a557f6e41805ccf5d3795cb149f30815b06
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-23 11:09:02 +0300

    Add ReactRouterLesson with BrowserRouter and Routes

---
+++ src/lessons/12-react-router/ReactRouterLesson.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function ReactRouterLesson() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <hr />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+        <Route path='/about' element={<About />} />
+
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default ReactRouterLesson;
