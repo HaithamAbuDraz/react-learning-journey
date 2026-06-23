9668cbb33ab6baa41e3ad656692bd83cc50501e8
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-23 14:25:48 +0300

    Add Users and UserProfile routes to React Router lesson

--- src/lessons/12-react-router/ReactRouterLesson.jsx
+++ src/lessons/12-react-router/ReactRouterLesson.jsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Users from './pages/Users';
+import UserProfile from './pages/UserProfile';
 
 function ReactRouterLesson() {
   return (
@@ -13,10 +15,10 @@ function ReactRouterLesson() {
 
       <Routes>
         <Route path='/' element={<Home />} />
-
         <Route path='/about' element={<About />} />
-
         <Route path='/contact' element={<Contact />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/users/:id' element={<UserProfile />} />{' '}
       </Routes>
     </BrowserRouter>
   );
