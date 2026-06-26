fc9e748e6684c846a47e28de376641021109ff8e
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-06-26 16:38:00 +0300

    add counterReducer with multiple action types

---
+++ src/lessons/14-useReducer/reducer/counterReducer.js
@@ -0,0 +1,14 @@
+export function counterReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    case 'RESET':
+      return { count: 0 };
+    case 'INCREMENT_BY':
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
