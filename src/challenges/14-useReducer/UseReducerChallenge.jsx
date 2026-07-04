07bdee1f4f8cbbaa5acb5b9960be27c85aaf75b0
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-07-04 11:21:55 +0300

    Add UseReducerChallenge wrapper for TodoApp

---
+++ src/challenges/14-useReducer/UseReducerChallenge.jsx
@@ -0,0 +1,16 @@
+import TodoApp from './components/TodoApp';
+
+function UseReducerChallenge() {
+  return (
+    <div>
+      <h1>useReducer Challenges</h1>
+
+      <section>
+        <h2>Challenge 1: Todo Reducer</h2>
+        <TodoApp />
+      </section>
+    </div>
+  );
+}
+
+export default UseReducerChallenge;
