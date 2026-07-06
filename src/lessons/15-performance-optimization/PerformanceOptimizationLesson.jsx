88d27971cef030a2951da2e30b9bc4a83cc0bf32
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-07-06 17:51:38 +0300

    Add PerformanceOptimizationLesson with Counter

---
+++ src/lessons/15-performance-optimization/PerformanceOptimizationLesson.jsx
@@ -0,0 +1,14 @@
+import Counter from './components/Counter';
+
+function PerformanceOptimizationLesson() {
+  return (
+    <div>
+      <h1>Performance Optimization</h1>
+
+      <Counter />
+
+    </div>
+  );
+}
+
+export default PerformanceOptimizationLesson;
